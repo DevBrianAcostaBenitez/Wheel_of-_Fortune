890a4cdd4e603b963f2dc0829cd5a6d1def0acc5
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-06 15:46:50 +0100

    advancement

--- public/js/remove.js
+++ public/js/remove.js
@@ -3,24 +3,32 @@ function remove() {
     var items = list.getElementsByTagName("li");
   
     for (var i = 0; i < items.length; i++) {
+      if (!items[i].querySelector(".removeButton")) {
       var removeButton = document.createElement("button");
       removeButton.innerHTML = "Remove";
-  
+      removeButton.classList.add("removeButton");
+      }
       removeButton.addEventListener("click", function() {
         var listItem = this.parentNode;
         var index = Array.prototype.indexOf.call(items, listItem);
-  
         list.removeChild(listItem);
-        hideRemoveButtons(); // Hide all remove buttons
+        hideRemoveButtons(); 
       });
   
       items[i].appendChild(removeButton);
     }
   }
-  
-  function hideRemoveButtons() {
-    var removeButtons = document.querySelectorAll("#list button");
-    for (var i = 0; i < removeButtons.length; i++) {
-      removeButtons[i].style.display = "none";
+  document.addEventListener("click", function(event) {
+    var target = event.target;
+    if (!target.matches(".removeButton") && !target.matches(".deleteButton")) {
+      removeEditButtons()
     }
+  })
+  function hideRemoveButtons() {
+    var removeButtons = document.querySelectorAll(".removeButton");
+    var list = document.getElementById("list");
+    var items = list.getElementsByTagName("li");
+    for (var i = 0; i < items.length; i++) {
+    items[i].removeChild(removeButtons);
+  }
   }
